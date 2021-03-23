1e049efd6d16dc3f83210d2b6077cd7b50da24aa
taeminahn <ahntaemin@naver.com>
2021-03-23 19:01:52 +0900

    asss-안태민-

--- Final-project/src/main/java/com/josso/main/MainController.java
+++ Final-project/src/main/java/com/josso/main/MainController.java
@@ -22,6 +22,7 @@ public class MainController {
 	// 혹시 안되는 사람 없쥬? 무야호~ 그만큼 신나신다는 거지
 	// 가자가자 무야호
 	// 나도 된다 - 수맹
+	// d
 
 	@RequestMapping(value="main", method = RequestMethod.GET)
 	public ModelAndView mainPage(ModelAndView modelAndView) {
